99c1c6683a029788f1297b25674b445f5283cc51
Mike Fix <mrfix84@gmail.com>
2017-01-16 22:55:02 -0600

    Create initial sanitizer

--- components/Sanitizer.jsx
+++ components/Sanitizer.jsx
@@ -4,18 +4,27 @@ import { DropTarget } from 'react-dnd'
 
 const fileTarget = {
   drop(props, monitor) {
-    console.log(monitor.getItem().files)
+    const { files } = monitor.getItem()
+    console.log(`convert ${files.map(f => f.path).join(' ')} file.pdf`)
   }
+}
+
+const style = {
+  background: 'grey',
+  margin: '0',
+  padding: '16px 24px',
+  width: '100%',
+  height: '50px'
 }
 
 class Sanitizer extends Component {
   render() {
-    const { connectDropTarget, isOver, canDrop } = this.props
+    const { connectDropTarget, isOver, canDrop, itemType, item } = this.props
     return connectDropTarget(
-      <div>
-        {!isOver && !canDrop && 'Drag files from the hard drive'}
-        {!isOver && canDrop && 'Drag the files here'}
-        {isOver && 'Drop the files'}
+      <div style={style}>
+        {!isOver && !canDrop && 'Grab an image file'}
+        {!isOver && canDrop && 'Drag your file to convert to PDF'}
+        {isOver && 'Drop'}
       </div>
     )
   }
@@ -24,5 +33,7 @@ class Sanitizer extends Component {
 export default DropTarget(NativeTypes.FILE, fileTarget, (connect, monitor) => ({
   connectDropTarget: connect.dropTarget(),
   isOver: monitor.isOver(),
-  canDrop: monitor.canDrop()
+  canDrop: monitor.canDrop(),
+  itemType: monitor.getItemType(),
+  item: monitor.getItem()
 }))(Sanitizer)
