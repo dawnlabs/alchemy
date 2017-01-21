23e8b4cde0ba4b1bb072c06d108cc18051487c76
Jake Dexheimer <jakedex@gmail.com>
2017-01-20 20:54:34 -0600

    Remove orange BG

--- src/components/Tray.jsx
+++ src/components/Tray.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Sanitizer from './Sanitizer'
 
-const { lightOrange: ORANGE } = require('../helpers/colors')
-
 const style = {
-  backgroundColor: ORANGE,
   width: '100vw',
   height: '100vh',
   display: 'flex'
