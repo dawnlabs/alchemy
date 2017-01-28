4e6a1aacf252e031740ece75ef18a0512adb0718
Jake Dexheimer <jakedex@gmail.com>
2017-01-27 22:03:37 -0600

    transGrey -> grey

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -5,7 +5,7 @@ const red = 'rgb(209, 75, 75)'
 const black = 'rgba(0,0,0,1)'
 const transBlack = 'rgba(0,0,0, 0.25)'
 const green = 'rgb(132, 255, 144)'
-const transGrey = 'rgba(130, 221, 240, 0.25)'
+const grey = '#F0F0EC'
 const orange = '#ee9a4c'
 const lightOrange = '#efb47c'
 
@@ -16,8 +16,8 @@ module.exports = {
   red,
   black,
   transBlack,
+  grey,
   green,
-  transGrey,
   orange,
   lightOrange
 }
