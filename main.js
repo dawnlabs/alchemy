5147b327790ae0a1c3e95da3c48f8b9056299103
Mike Fix <mrfix84@gmail.com>
2017-02-23 20:06:49 -0600

    Add brew install

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const menubar = require('menubar')
 
 const configure = require('./src/helpers/configure')
+const { installImageMagick } = require('./src/api')
 
 const mb = menubar({
   alwaysOnTop: true,
@@ -15,4 +16,6 @@ const { app } = mb
 mb.on('ready', () => {
   console.log('App started in menu bar.')
   configure(mb)
+
+  installImageMagick().then(console.log).catch(console.log)
 })
--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const execS = require('child_process').exec
 const { replaceSpaceCharacters } = require('./helpers/util')
 
 const convert = ({ files, outputPath, name }) => {
+  // eslint-disable-next-line dot-notation
   process.env['PATH'] = '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
 
   return new Promise((resolve, reject) => {
@@ -26,12 +27,15 @@ const convert = ({ files, outputPath, name }) => {
 const installImageMagick = () => {
   return new Promise((resolve, reject) => {
     execS('which brew', (error) => {
-      if (error) reject(new Error('Brew is required to run Alchemy. Please visit https://brew.sh/ to install.'))
-      else {
-        execS('brew install imagemagick', (error) => {
-          return error ? reject(error) : resolve(true)
-        })
-      }
+      if (error) return reject(new Error('Brew is required to run Alchemy. Please visit https://brew.sh/ to install.'))
+      return execS('which convert', (error) => {
+        if (error) {
+          return execS('brew install imagemagick', (error) => {
+            return error ? reject(error) : resolve('ImageMagick is installed.')
+          })
+        }
+        return resolve('ImageMagick already installed.')
+      })
     })
   })
 }
