6d41ebe01c4a25bcb799cce1ae3050830bda2ca5
Jake Dexheimer <jakedex@gmail.com>
2017-04-06 18:11:52 -0500

    Fix inconsistent colors

--- src/components/svg/Convert.jsx
+++ src/components/svg/Convert.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Convert = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
     <g fill="none" fillRule="evenodd">
-      <rect width="7.68" height="7.68" y="4.32" fill="#1D8CF8" rx=".5" />
-      <rect width="7.68" height="7.68" x="4.32" fill="#80C772" rx=".5" />
+      <rect width="7.68" height="7.68" y="4.32" fill="#3B86FF" rx=".5" />
+      <rect width="7.68" height="7.68" x="4.32" fill="#4DB15E" rx=".5" />
     </g>
   </svg>
 )
