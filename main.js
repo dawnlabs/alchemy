55224573d9c6591630ede9f69d374ed6f5bf6495
briandennis <briandennis321@gmail.com>
2017-04-13 17:26:13 -0500

    fix main

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const menubar = require('menubar')
 
 const configure = require('./src/helpers/configure')
-const { installImageMagick } = require('./src/api')
 
 const mb = menubar({
   alwaysOnTop: true,
@@ -16,6 +15,4 @@ const { app } = mb
 mb.on('ready', () => {
   console.log('App started in menu bar.')
   configure(mb)
-
-  installImageMagick().then(console.log).catch(console.log)
 })
