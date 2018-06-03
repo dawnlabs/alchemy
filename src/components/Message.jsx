447f122165019562ed4f0c71ddfb1b15ef5e1093
Mike Fix <mrfix84@gmail.com>
2018-06-03 16:24:45 -0700

    Clean up Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,40 +1,28 @@
 import React from 'react'
 
+const Card = ({ title, detail }) => (
+  <div>
+    <h1>{title}</h1>
+    <p className="detail">{detail}</p>
+  </div>
+)
+
 const Message = ({ hover, state }) => {
   switch (state.status) {
     case 'IDLE': return (
-      <div>
-        <h1>
-          { hover ? 'Drop' : 'Drag & drop' }
-        </h1>
-        <p className="detail">
-          {`your files here to ${state.shifted ? 'convert' : 'add them'}`}
-        </p>
-      </div>
+      <Card
+        title={hover ? 'Drop' : 'Drag & drop'}
+        detail={`your files here to ${state.shifted ? 'convert' : 'add them'}`}
+      />
     )
     case 'CONVERTING': return (
-      <div>
-        <h1>
-          Converting
-        </h1>
-        <p className="detail">(this should only take a sec)</p>
-      </div>
+      <Card title="Converting" detail="(this should only take a sec)" />
     )
     case 'DONE': return (
-      <div>
-        <h1>
-          Complete!
-        </h1>
-        <p className="detail">{`${state.fileName || 'File'} created`}</p>
-      </div>
+      <Card title="Complete!" detail={`${state.fileName || 'File'} created`} />
     )
     case 'FAILED': return (
-      <div>
-        <h1>
-          Conversion failed
-        </h1>
-        <p className="detail">Uh oh, something went wrong 😕</p>
-      </div>
+      <Card title="Conversion failed" detail="Uh oh, something went wrong 😕" />
     )
     default: return null
   }
